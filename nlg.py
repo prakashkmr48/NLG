e8c5c7454f3fc225a82bcf91720d795e6513ac05
prakashkmr48 <prakashkmr48@gmail.com>
2024-01-16 23:21:39 +0530

    Update nlg.py to load relevant content only

--- nlg.py
+++ nlg.py
@@ -2,13 +2,17 @@ import streamlit as st
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
 
-# Load pre-trained GPT-2 model and tokenizer
-model_name = 'gpt2'
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
-model = GPT2LMHeadModel.from_pretrained(model_name)
+# Function to load GPT-2 model and tokenizer
+@st.cache(allow_output_mutation=True)
+def load_model_and_tokenizer():
+    model_name = 'gpt2'
+    tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+    model = GPT2LMHeadModel.from_pretrained(model_name)
+    return model, tokenizer
 
 # Function for text generation using GPT-2
-def generate_text(prompt, max_length=100):
+@st.cache(allow_output_mutation=True)
+def generate_text(prompt, model, tokenizer, max_length=100):
     input_ids = tokenizer.encode(prompt, return_tensors='pt')
 
     # Generate text
@@ -20,12 +24,15 @@ def generate_text(prompt, max_length=100):
 # Streamlit app
 st.title("GPT-2 Text Generation App")
 
+# Load GPT-2 model and tokenizer
+model, tokenizer = load_model_and_tokenizer()
+
 # Get user input prompt
 user_prompt = st.text_input("Enter your prompt:")
 
 if st.button("Generate Text"):
     # Generate response
-    generated_response = generate_text(user_prompt)
+    generated_response = generate_text(user_prompt, model, tokenizer)
 
     # Display generated response
     st.subheader("Generated Response:")
